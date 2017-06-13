acb3341589c5b120164a4a3812897cf45b4df7c4
Jorge <jlmadrid360@gmail.com>
2017-06-13 16:22:03 -0500

    cosults the current state of a dispatched sms

--- lib/elibom_ex/client.ex
+++ lib/elibom_ex/client.ex
@@ -4,26 +4,40 @@ defmodule ElibomEx.Client do
   """
   alias ElibomEx.Config
 
+  @typedoc """
+  Describes the different type of responses from Elibom's API
+  """
+  @type http_succeed :: {:ok, map()}
+  @type http_error_in_request :: {:error, map(), number()}
+  @type http_error_calling_service :: {:error, String.t}
+
   @spec deliver_sms(%Config{}, map) ::
     {:ok, map()} | {:error, String.t} | {:error, String.t}
   def deliver_sms(config, request_body) do
-    case perform_request(:post, config, "/messages", request_body) do
-      {:ok, %HTTPoison.Response{body: body, headers: _, status_code: 200}} ->
-        {:ok, Poison.decode!(body)}
-      {:ok, %HTTPoison.Response{body: body, headers: _, status_code: status_code}} when status_code >= 400 ->
-        {:error, body, code: status_code}
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, "Unable to comunicate with Elibom service. Reason: #{reason}"}
-    end
+    perform_request(:POST, config, "/messages", request_body)
   end
 
-  defp perform_request(method, config, service, request_body) do
+  @spec consult_delivery(%Config{}, String.t) ::
+    http_succeed() | http_error_in_request() | http_error_calling_service()
+  def consult_delivery(_config, nil), do: raise ArgumentError, message: "delivery_id cannot be empty or nil"
+  def consult_delivery(config, delivery_id) do
+    perform_request(:GET, config, "messages/#{delivery_id}")
+  end
+
+  defp perform_request(method, config, service, request_body \\ nil) do
     url = URI.parse(config.domain <> service)
     auth_token = Base.encode64("#{config.username}:#{config.password}")
     headers = [{"Accept", "application/json"}, {"Authorization", "Basic #{auth_token}"}]
 
     body = Poison.encode!(request_body)
 
-    HTTPoison.request(method, url, body, headers)
+    case HTTPoison.request(method, url, body, headers) do
+      {:ok, %HTTPoison.Response{body: body, headers: _, status_code: 200}} ->
+        {:ok, Poison.decode!(body)}
+      {:ok, %HTTPoison.Response{body: body, headers: _, status_code: status_code}} when status_code >= 400 ->
+        {:error, Poison.decode!(body), status_code}
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, "Unable to comunicate with Elibom service. Reason: #{reason}"}
+    end
   end
 end
--- test/elibom_ex/client_test.exs
+++ test/elibom_ex/client_test.exs
@@ -9,7 +9,37 @@ defmodule ElibomEx.ClientTest do
         response =
           Client.deliver_sms(Config.build!, %{to: "573142272251", text: "Sample SMS"})
 
-          assert response == {:ok, %{"deliveryToken" => "1234567890"}}
+          assert response == {:ok, %{"deliveryToken" => "1857014352691220612"}}
+      end
+    end
+  end
+
+  describe "consult_delivery/2" do
+    test "fetch the current state of a delivered SMS" do
+      use_cassette "consult_delivered_sms" do
+        {:ok, response} =
+          Client.consult_delivery(Config.build!, "1857014352691220612")
+
+        assert Map.has_key?(response, "deliveryId") == true
+        assert Map.has_key?(response, "messages") == true
+        assert Map.has_key?(response, "numSent") == true
+      end
+    end
+
+    test "raise an ArgumentError Exception without a provided delivery id" do
+      assert_raise ArgumentError, fn() ->
+        Client.consult_delivery(Config.build!, nil)
+      end
+    end
+
+    test "returns http 404 status if the delivery id does not exists" do
+      use_cassette "elibom_not_found_request" do
+        {:error, response, status_code} =
+          Client.consult_delivery(Config.build!, "fake_delivery_id")
+
+        assert response["code"] == "not_found"
+        assert response["description"] != nil
+        assert status_code == 404
       end
     end
   end
