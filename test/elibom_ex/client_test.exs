6c349305d648950f6fa917750346e147395ad663
Jorge <jlmadrid360@gmail.com>
2017-06-13 16:52:25 -0500

    Add spec to delayed sms deliveries

--- test/elibom_ex/client_test.exs
+++ test/elibom_ex/client_test.exs
@@ -4,21 +4,42 @@ defmodule ElibomEx.ClientTest do
   alias ElibomEx.{Config, Client}
 
   describe "deliver_sms/2" do
-    test "delivers a sms" do
+
+    setup do
+      {:ok, config: Config.build!}
+    end
+
+    test "delivers a sms", %{config: config} do
       use_cassette "dispatched_elibom_sms" do
         response =
-          Client.deliver_sms(Config.build!, %{to: "573142272251", text: "Sample SMS"})
+          Client.deliver_sms(config, %{to: "573142222222", text: "Sample SMS"})
 
           assert response == {:ok, %{"deliveryToken" => "1857014352691220612"}}
+      end
+    end
+
+    test "function supports message scheduling", %{config: config} do
+      use_cassette "dispatched_scheduled_sms" do
+        response =
+          Client.deliver_sms(
+            config,
+            %{to: "573142222222", text: "Sample SMS", "scheduleDate": "2017-06-18 19:10"}
+          )
+
+        assert response == {:ok, %{"scheduleId" => "1410585"}}
       end
     end
   end
 
   describe "consult_delivery/2" do
-    test "fetch the current state of a delivered SMS" do
+    setup do
+      {:ok, config: Config.build!}
+    end
+
+    test "fetch the current state of a delivered SMS", %{config: config} do
       use_cassette "consult_delivered_sms" do
         {:ok, response} =
-          Client.consult_delivery(Config.build!, "1857014352691220612")
+          Client.consult_delivery(config, "1857014352691220612")
 
         assert Map.has_key?(response, "deliveryId") == true
         assert Map.has_key?(response, "messages") == true
@@ -26,16 +47,20 @@ defmodule ElibomEx.ClientTest do
       end
     end
 
-    test "raise an ArgumentError Exception without a provided delivery id" do
+    test "raise an ArgumentError Exception without a provided delivery id",
+      %{config: config} do
+
       assert_raise ArgumentError, fn() ->
-        Client.consult_delivery(Config.build!, nil)
+        Client.consult_delivery(config, nil)
       end
     end
 
-    test "returns http 404 status if the delivery id does not exists" do
+    test "returns http 404 status if the delivery id does not exists",
+      %{config: config} do
+
       use_cassette "elibom_not_found_request" do
         {:error, response, status_code} =
-          Client.consult_delivery(Config.build!, "fake_delivery_id")
+          Client.consult_delivery(config, "fake_delivery_id")
 
         assert response["code"] == "not_found"
         assert response["description"] != nil
