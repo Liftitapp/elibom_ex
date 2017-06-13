e4b1f04677d8962511227f2dbf072853e931be59
Jorge <jlmadrid360@gmail.com>
2017-06-13 16:37:22 -0500

    Improves documentation

--- lib/elibom_ex/client.ex
+++ lib/elibom_ex/client.ex
@@ -11,12 +11,19 @@ defmodule ElibomEx.Client do
   @type http_error_in_request :: {:error, map(), number()}
   @type http_error_calling_service :: {:error, String.t}
 
+  @doc """
+  Requests to Elibom's service to dispatch a new sms
+  """
   @spec deliver_sms(%Config{}, map) ::
     {:ok, map()} | {:error, String.t} | {:error, String.t}
   def deliver_sms(config, request_body) do
     perform_request(:POST, config, "/messages", request_body)
   end
 
+  @doc """
+  Consults the current state of an already sent sms.
+  Raises `ArgumentError`exception if the delivery_id is nil or empty
+  """
   @spec consult_delivery(%Config{}, String.t) ::
     http_succeed() | http_error_in_request() | http_error_calling_service()
   def consult_delivery(_config, nil), do: raise ArgumentError, message: "delivery_id cannot be empty or nil"
